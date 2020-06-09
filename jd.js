a7bd529d3f530688776f3cf15e8acefd56f22119
Jagdishsinghdhami <66659901+Jagdishsinghdhami@users.noreply.github.com>
2020-06-09 13:31:24 +0530

    Add files via upload

---
+++ jd.js
@@ -0,0 +1,9 @@
+document.querySelector("button").addEventListener("click", function(){
+  document.body.style.background=randcolor();
+})
+function randcolor(){
+  return '#'+(function co(lor){return(lor [0,1,2,3,4,4,5,6,7,8,9,'a', 'b', 'c','d', 'e','f']
+  [Math.floor(math.random()*16)])
+  &&(lor.length==6)?lor:co(lor){}
+  })
+}*/
